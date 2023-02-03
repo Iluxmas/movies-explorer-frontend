6470b9d8c0e7120f1463318460d373c30c097bc8
Ilya Sulkhanov <masta.pxty@gmail.com>
2023-02-03 13:06:44 +0300

    fix UNAUTH redirection to main page

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 export default function ProtectedRoute(props) {
-  return <Route>{() => (props.isLogged ? props.children : <Redirect to='./sign-in' />)}</Route>;
+  return <Route>{() => (props.isLogged ? props.children : <Redirect to='./' />)}</Route>;
 }
